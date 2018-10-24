0e377a1cd7b647ebb8d82a2b74c98dbd1be431af
Frisly <frisly@playfrisly.com>
2018-10-24 18:51:39 -0400

    update video references

--- src/videos.js
+++ src/videos.js
@@ -1,10 +1,30 @@
 export default [
   {
-    name: 'Deck Vignettes',
-    url: 'https://vimeo.com/294702884',
+    name: 'Lets Clear the Air',
+    url: 'https://vimeo.com/289412607',
   },
   {
-    name: 'Dame tu Cosita: Spiderman',
-    url: 'https://vimeo.com/265118268',
+    name: 'Nature & Warehouse',
+    url: 'https://vimeo.com/289415952',
+  },
+  {
+    name: 'Familia Jaime',
+    url: 'https://vimeo.com/294699711/94e7222de9',
+  },
+  {
+    name: 'Making Nopales',
+    url: 'https://vimeo.com/289413239',
+  },
+  //{
+  //  name: 'Night Scene',
+  //  url: '',
+  //},
+  {
+    name: 'Fleeting Mechanical Dreams',
+    url: 'https://vimeo.com/289414160',
+  },
+  {
+    name: 'Josh',
+    url: 'https://vimeo.com/294702884',
   },
 ];
