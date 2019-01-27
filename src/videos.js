45a788fac88eadc85c70a37b945f80d6eb364be2
Frisly <34254858+frisly@users.noreply.github.com>
2019-01-27 18:11:19 -0500

    Update videos.js

--- src/videos.js
+++ src/videos.js
@@ -4,16 +4,8 @@ export default [
     url: 'https://youtu.be/1WVUbO1VbeQ',
   },
   {
-    name: 'Area',
-    url: 'https://youtu.be/FWe0k-HyJwA ',
-  },
-  {
     name: 'Familia Jaime',
     url: 'https://youtu.be/OaWzi4Q97fI',
-  },
-  {
-    name: 'Making Nopales',
-    url: 'https://vimeo.com/296980588',
   },
   
 ];
