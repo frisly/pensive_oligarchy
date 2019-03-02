e661a7e57bddc6efcb94c1511abb8d174f481af3
Frisly <34254858+frisly@users.noreply.github.com>
2019-03-01 22:16:51 -0500

    Update videos.js

--- src/videos.js
+++ src/videos.js
@@ -3,9 +3,5 @@ export default [
     name: 'Lets Clear the Air',
     url: 'https://youtu.be/1WVUbO1VbeQ',
   },
-  {
-    name: 'Familia Jaime',
-    url: 'https://youtu.be/OaWzi4Q97fI',
-  },
   
 ];
